be615fcf12c343d2369b23c51ae54eb44aadddc4
刘旋尧 <536464346@qq.com>
2018-08-31 09:17:34 +0800

    format

--- src/index.js
+++ src/index.js
@@ -3,178 +3,178 @@ import cheerio from 'cheerio'
 import Bottleneck from 'bottleneck'
 
 export class HtmlElement {
-    constructor({
-                    tag,
-                    $
-                }) {
-        this.tag = tag
-        this.$ = $
-    }
+  constructor({
+                tag,
+                $
+              }) {
+    this.tag = tag
+    this.$ = $
+  }
 }
 
 
 export class LimitRule {
-    constructor({
-                    maxConnections = 0,
-                    delayTime = 0,
-                    highWater = -1,
-                }) {
-        this.maxConcurrent = maxConnections
-        this.minTime = delayTime
-        this.highWater = highWater
-        this.strategy = Bottleneck.strategy.LEAK
-        this.rejectOnDrop = false
-    }
+  constructor({
+                maxConnections = 0,
+                delayTime = 0,
+                highWater = -1,
+              }) {
+    this.maxConcurrent = maxConnections
+    this.minTime = delayTime
+    this.highWater = highWater
+    this.strategy = Bottleneck.strategy.LEAK
+    this.rejectOnDrop = false
+  }
 }
 
 
 class Request {
-    constructor({
-                    tag,
-                    requestOption,
-                    url
-                }) {
-        this.tag = tag
-        this.requestOption = requestOption
-        this.url = url
+  constructor({
+                tag,
+                requestOption,
+                url
+              }) {
+    this.tag = tag
+    this.requestOption = requestOption
+    this.url = url
 
-    }
+  }
 }
 
 class Response {
-    constructor({
-                    tag,
-                    responseData
-                }) {
-        this.tag = tag
-        this.data = responseData
-    }
+  constructor({
+                tag,
+                responseData
+              }) {
+    this.tag = tag
+    this.data = responseData
+  }
 }
 
 export default class nspider {
 
 
-    constructor({
-                    name = '',
-                    headers = {}
-                }) {
-        this.name = name
-        this.htmlCallBacks = new Map();
-        this.requestCallBacks = []
-        this.responseCallBacks = []
-        this.headers = headers
-        this.nrequest = new nrequest()
-        this.limitRule = new LimitRule({})
+  constructor({
+                name = '',
+                headers = {}
+              }) {
+    this.name = name
+    this.htmlCallBacks = new Map();
+    this.requestCallBacks = []
+    this.responseCallBacks = []
+    this.headers = headers
+    this.nrequest = new nrequest()
+    this.limitRule = new LimitRule({})
 
-        this.limiter = new Bottleneck(this.limitRule.maxConcurrent, this.limitRule.minTime)
+    this.limiter = new Bottleneck(this.limitRule.maxConcurrent, this.limitRule.minTime)
+  }
+
+
+  visit(url, tag = '') {
+    this.scrape(url, 'GET', tag)
+
+  }
+
+  post(url, tag) {
+    this.scrape(url, 'POST', tag)
+  }
+
+  async scrape(url, method, tag = '') {
+
+    const options = {}
+
+    options.method = method
+    options.headers = this.headers
+    options.url = url
+
+
+    if (this.requestCallBacks.length > 0) {
+      const req = new Request({
+        tag,
+        requestOption: options,
+        url
+      })
+      this.handleRequest(req)
     }
 
+    const data = await this.limiter.schedule(this.nrequest.nrequest, options)
 
-    visit(url, tag = '') {
-        this.scrape(url, 'GET', tag)
+    if (this.responseCallBacks.length > 0) {
+      const res = new Response({
+        tag,
+        responseData: data
+      })
+      this.handleResponse(res)
+    }
+    if (this.htmlCallBacks.size > 0) {
+      const $ = cheerio.load(data)
+      this.handleHtml({
+        tag,
+        $
+      })
+    }
+  }
 
+  setHeaders(headers) {
+    for (let key in headers) {
+      this.setHeader(key, headers[key])
     }
 
-    post(url, tag) {
-        this.scrape(url, 'POST', tag)
+    return this
+  }
+
+  setHeader(key, val) {
+    this.headers[key] = val
+    return this
+  }
+
+
+  setLimiter(limitRule) {
+    this.limitRule = limitRule
+    this.limiter = new Bottleneck(this.limitRule.maxConcurrent, this.limitRule.minTime)
+    return this
+  }
+
+
+  onHtml(selector, cb) {
+    this.htmlCallBacks.set(selector, cb)
+  }
+
+  onRequest(cb) {
+    this.requestCallBacks.push(cb)
+  }
+
+
+  onResponse(cb) {
+    this.responseCallBacks.push(cb)
+  }
+
+
+  handleRequest(req) {
+    for (let cb of this.requestCallBacks) {
+      cb(req)
     }
+  }
 
-    async scrape(url, method, tag = '') {
-
-        const options = {}
-
-        options.method = method
-        options.headers = this.headers
-        options.url = url
-
-
-        if (this.requestCallBacks.length > 0) {
-            const req = new Request({
-                tag,
-                requestOption: options,
-                url
-            })
-            this.handleRequest(req)
-        }
-
-        const data = await this.limiter.schedule(this.nrequest.nrequest, options)
-
-        if (this.responseCallBacks.length > 0) {
-            const res = new Response({
-                tag,
-                responseData: data
-            })
-            this.handleResponse(res)
-        }
-        if (this.htmlCallBacks.size > 0) {
-            const $ = cheerio.load(data)
-            this.handleHtml({
-                tag,
-                $
-            })
-        }
+  handleResponse(res) {
+    for (let cb of this.responseCallBacks) {
+      cb(res)
     }
+  }
 
-    setHeaders(headers) {
-        for (let key in headers) {
-            this.setHeader(key, headers[key])
-        }
-
-        return this
+  handleHtml({
+               tag,
+               $
+             }) {
+    for (let [selector, cb] of this.htmlCallBacks) {
+      const objects = $(selector)
+      for (let i = 0; i < objects.length; i++) {
+        const element = new HtmlElement({
+          tag,
+          $: $(objects[i])
+        })
+        cb(element)
+      }
     }
-
-    setHeader(key, val) {
-        this.headers[key] = val
-        return this
-    }
-
-
-    setLimiter(limitRule) {
-        this.limitRule = limitRule
-        this.limiter = new Bottleneck(this.limitRule.maxConcurrent, this.limitRule.minTime)
-        return this
-    }
-
-
-    onHtml(selector, cb) {
-        this.htmlCallBacks.set(selector, cb)
-    }
-
-    onRequest(cb) {
-        this.requestCallBacks.push(cb)
-    }
-
-
-    onResponse(cb) {
-        this.responseCallBacks.push(cb)
-    }
-
-
-    handleRequest(req) {
-        for (let cb of this.requestCallBacks) {
-            cb(req)
-        }
-    }
-
-    handleResponse(res) {
-        for (let cb of this.responseCallBacks) {
-            cb(res)
-        }
-    }
-
-    handleHtml({
-                   tag,
-                   $
-               }) {
-        for (let [selector, cb] of this.htmlCallBacks) {
-            const objects = $(selector)
-            for (let i = 0; i < objects.length; i++) {
-                const element = new HtmlElement({
-                    tag,
-                    $: $(objects[i])
-                })
-                cb(element)
-            }
-        }
-    }
+  }
 }
--- src/request.js
+++ src/request.js
@@ -4,69 +4,69 @@ import zlib from 'zlib'
 
 export default class Nrequest {
 
-    constructor() {
+  constructor() {
 
-    }
+  }
 
-    nrequest(options) {
-        const req = request(options);
+  nrequest(options) {
+    const req = request(options);
 
-        return new Promise((resolve) => {
+    return new Promise((resolve) => {
 
-            req.on('response', (res) => {
-                var chunks = [];
-                res.on('data', (chunk) => {
-                    chunks.push(chunk);
-                });
+      req.on('response', (res) => {
+        var chunks = [];
+        res.on('data', (chunk) => {
+          chunks.push(chunk);
+        });
 
-                res.on('end', () => {
-                    var buffer = Buffer.concat(chunks);
-                    var encoding = res.headers['content-encoding'];
-                    if (encoding == 'gzip') {
-                        zlib.gunzip(buffer, (err, decoded) => {
-                            if (err) {
-                                reject(err)
-                            } else {
-                                resolve(decoded && decoded.toString())
-                            }
-
-                        });
-                    } else if (encoding == 'deflate') {
-                        zlib.inflate(buffer, (err, decoded) => {
-                            if (err) {
-                                reject(err)
-                            } else {
-                                resolve(decoded && decoded.toString())
-                            }
-                        })
-                    } else {
-                        resolve(buffer.toString())
-                    }
-                });
-            });
-
-            req.on('error', (err) => {
+        res.on('end', () => {
+          var buffer = Buffer.concat(chunks);
+          var encoding = res.headers['content-encoding'];
+          if (encoding == 'gzip') {
+            zlib.gunzip(buffer, (err, decoded) => {
+              if (err) {
                 reject(err)
+              } else {
+                resolve(decoded && decoded.toString())
+              }
+
             });
+          } else if (encoding == 'deflate') {
+            zlib.inflate(buffer, (err, decoded) => {
+              if (err) {
+                reject(err)
+              } else {
+                resolve(decoded && decoded.toString())
+              }
+            })
+          } else {
+            resolve(buffer.toString())
+          }
+        });
+      });
+
+      req.on('error', (err) => {
+        reject(err)
+      });
 
 
-        })
+    })
 
 
+  }
+
+  async doRequest(options) {
+    let err, data = null
+    try {
+      data = await this.nrequest(options)
+    } catch (e) {
+      err = e
+    }
+    return {
+      err,
+      data
     }
 
-    async doRequest(options) {
-        let err, data = null
-        try {
-            data = await this.nrequest(options)
-        } catch (e) {
-            err = e
-        }
-        return {
-            err,
-            data
-        }
 
-
-    }
+  }
 }
